231442aba988746359245ad713d67066afe772fd
victor <vicpalpes@alu.edu.gva.es>
2026-03-09 10:13:08 +0100

    cambio_contrasenyas

--- 3_javascript/js/app4.js
+++ 3_javascript/js/app4.js
@@ -10,16 +10,19 @@ function numAleatorio(min,max){
 }
 
 let password=""
+let aleatorio=numAleatorio(8,50)
 
-for (i=0;i<50;i++){
+while (password.length <= aleatorio){
 let num=caracteresNumeros[numAleatorio(0,9)]
 let letraEspecial=caracteresEspeciales[numAleatorio(0,30)]
 let letras=caracteres[numAleatorio(0,26)]
 let letrasMayus=caracteresMayusculas[numAleatorio(0,26)]
-password=num+letraEspecial+letras+letrasMayus
+password=password+num+letraEspecial+letras+letrasMayus
 }
 
 //desordenar array
 password=password.split('').sort(() => Math.random() - 0.5).join('')   
 
 console.log(password)
+
+
