41cfe88038af4fc69ab541169f7816131a05c645
victor <vicpalpes@alu.edu.gva.es>
2026-04-28 12:30:20 +0200

    cambios en practica js

--- 3_javascript/js/app2.js
+++ 3_javascript/js/app2.js
@@ -5,11 +5,12 @@ function dameNumeros(max){
     return num
 }
 
-let veces=1
+// let veces=1
 let numeros=[]
 let aqui=document.getElementById("solucion")
 
 while(numeros.length<6){
+    //si utilizamos veces se pondría veces<=6 con un for
     let variable=dameNumeros(49)
     if(numeros.indexOf(variable)>-1){
     }
